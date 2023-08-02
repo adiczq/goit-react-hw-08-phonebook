76b077c2d5719c17c84ea118afc8198865b200f7
Adrian Lacheta <adiczq@gmail.com>
2023-08-02 12:02:40 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
+import { useAuth } from '../../hooks';
 
 export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <nav>
       <NavLink className={css.link} to="/">
         Home
       </NavLink>
-      <NavLink className={css.link} to="/phonebook">
-        Phonebook
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/tasks">
+          Tasks
+        </NavLink>
+      )}
     </nav>
   );
 };
