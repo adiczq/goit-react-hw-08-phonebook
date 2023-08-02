45a3337d281c851bd425b6a0f82fd561d17845c7
Adrian Lacheta <adiczq@gmail.com>
2023-08-02 11:33:04 +0200

    Update selectors.js

--- src/redux/slices/selectors.js
+++ src/redux/slices/selectors.js
@@ -1,2 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.contacts;
-export const selectFilter = state => state.contacts.filter;
+export const selectFilterValue = state => state.filter;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilterValue],
+  (contacts, filterValue) => {
+    if (filterValue === '') {
+      return contacts;
+    } else {
+      return contacts.filter(
+        contact =>
+          contact.name
+            .toLowerCase()
+            .includes(filterValue.filter.toLowerCase()) ?? []
+      );
+    }
+  }
+);
